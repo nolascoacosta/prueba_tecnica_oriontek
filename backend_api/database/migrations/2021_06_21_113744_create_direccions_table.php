564af821dcd412625b948ecea03ff5da4e9eb43c
Ramon Nolasco <ramonnolascoacosta@gmail.com>
2021-06-21 08:20:41 -0400

    migraciones para empresas, clientes y direcciones

---
+++ backend_api/database/migrations/2021_06_21_113744_create_direccions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDireccionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('direccions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->string('calle');
+            $table->string('num_dep');
+            $table->string('codigo_postal');
+            $table->string('sector');
+            $table->string('municipio');
+            $table->string('provincia');
+            $table->timestamps();
+
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('direccions');
+    }
+}
